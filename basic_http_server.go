30db862961ce37fdbed55b02795e7ea0ad193ba0
DanielHauge <Animcuil@gmail.com>
2017-09-18 15:47:25 +0200

    Update basic_http_server.go

--- basic_http_server.go
+++ basic_http_server.go
@@ -15,7 +15,7 @@ type Person struct {
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	
-		fmt.Fprintln(w, "Hello Static website") 
+		fmt.Fprintln(w, "Hello Static website - Og lidt mere") 
 	
 
 	
