ebf2f66de03e495ed538be26390820f54b86f32a
DanielHauge <Animcuil@gmail.com>
2017-09-18 15:39:14 +0200

    Update basic_http_server.go

--- basic_http_server.go
+++ basic_http_server.go
@@ -15,41 +15,8 @@ type Person struct {
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	
-	//Gets Session
-	session, err := mgo.Dial("192.168.20.3:27017")
-	if err != nil {
-			panic(err)
-	}
-	defer session.Close()
-
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
-
-	c := session.DB("test").C("people")
-	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
-			   &Person{"Cla", "+55 53 8402 8510"})
-	if err != nil {
-			log.Fatal(err)
-	}
-
-	/// Queres persons of with these parameters.
-	// result := Person{}
-	// err = c.Find(bson.M{"name": "Ale"}).One(&result)
-	// if err != nil {
-	// 		log.Fatal(err)
-	// }
-
-	// fmt.Println("Phone:", result.Phone)
-
+		fmt.Fprintln(w, "Hello Static website") 
 	
-	var results []Person
-	
-	error := c.Find(nil).All(&results)
-	if error != nil {
-			// TODO: Do something about the error
-	} else {
-		fmt.Fprintln(w, results) 
-	}
 
 	
 
@@ -61,7 +28,7 @@ func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	port := 8866
+	port := 8080
 
 	http.HandleFunc("/", helloWorldHandler)
 
