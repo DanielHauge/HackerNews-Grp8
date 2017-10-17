55155403367b00bc66b03fd59e9b1e071f0e91e3
DanielHauge <Animcuil@gmail.com>
2017-10-17 23:29:30 +0200

    Update deploy2.sh

--- Production/GoAPI/deploy2.sh
+++ Production/GoAPI/deploy2.sh
@@ -2,9 +2,11 @@
 
 BUILD_NUMBER=$1
 DOCKER_ID=$2
+DOCKER_IMAGE=$3
+DOCKER_PORTS=$4
 # stop all running containers with our web application
-docker stop `docker ps -a | grep ${DOCKER_ID}/goapi | awk '{print substr ($0, 0, 12)}'`
+docker stop `docker ps -a | grep ${DOCKER_ID}/${DOCKER_IMAGE} | awk '{print substr ($0, 0, 12)}'`
 # remove all of those containers
-docker rm `docker ps -a | grep ${DOCKER_ID}/goapi | awk '{print substr ($0, 0, 12)}'`
-docker pull ${DOCKER_ID}/gotestsite:${BUILD_NUMBER}
-docker run -d -ti -p 8787:8787 ${DOCKER_ID}/goapi:${BUILD_NUMBER}
+docker rm `docker ps -a | grep ${DOCKER_ID}/${DOCKER_IMAGE} | awk '{print substr ($0, 0, 12)}'`
+docker pull ${DOCKER_ID}/${DOCKER_IMAGE}:${BUILD_NUMBER}
+docker run -d -ti -p ${DOCKER_PORTS} ${DOCKER_ID}/${DOCKER_IMAGE}:${BUILD_NUMBER}
