2cd59b9ad25cf4d71fc21026909afb1b5fc03fe7
DanielHauge <Animcuil@gmail.com>
2017-11-03 20:24:06 +0100

    Fixed parse issue

--- Production/GoAPIWeb/Main.go
+++ Production/GoAPIWeb/Main.go
@@ -31,7 +31,7 @@ func main() {
 
 
 	log.Println("Initializing Database Connection.")
-	db, err := sql.Open("mysql", os.Args[1]+":"+os.Args[2]+"@tcp("+os.Args[3]+":3306)/HackerNewsDB")
+	db, err := sql.Open("mysql", os.Args[1]+":"+os.Args[2]+"@tcp("+os.Args[3]+":3306)/HackerNewsDB?parseTime=True")
 	if err != nil {
 		fmt.Print(err.Error())
 	}
